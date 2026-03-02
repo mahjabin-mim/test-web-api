eff59c5d34accb832583f031729019bb08c76a46
Mahjabin Mim <mahjabinmim711@gmail.com>
2026-03-02 15:10:54 +0600

    Add files via upload

---
+++ my-web-api/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace my_web_api.Controllers
+{
+    // [Route("api/[controller]")]
+    [Route("api")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpGet("health")]
+        public IActionResult Health()
+        {
+            return Ok("Created Endpoint"); 
+        }
+    }
+}
+
